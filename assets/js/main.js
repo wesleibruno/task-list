5d2d18413ce65e4eecb59b8a657cb9c536267e99
Weslei Bruno <weslei945@gmail.com>
2020-09-26 23:13:17 -0300

    second commit

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+//1 - selecionar o input
+//2 - selecionar o botão de adicionar tarefa
+//3 - selecionar ul
+//4 - capturar evento de click no botão de adicionar tarefas
+
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+inputTarefa.addEventListener('keypress', function(e) {
+    if(e.keyCode === 13) {
+        if(!inputTarefa.value ) return;
+        criaTarefa(inputTarefa.value);
+    }
+});
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    li.appendChild(botaoApagar);
+}
+
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+}
+
+btnTarefa.addEventListener('click', function () {
+    if(!inputTarefa.value ) return;
+    criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    // console.log(el)
+
+    if(el.classList.contains('apagar')) {
+        console.log('apagar clicado');
+    }
+})
